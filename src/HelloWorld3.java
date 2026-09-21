c9abf5be947039b3a7cc649af169435f08b9ed7f
s1naoui <wi26b186@technikum-wien.at>
2026-09-21 15:52:05 +0200

    Initial import of data reader and JSON dataset

---
+++ src/HelloWorld3.java
@@ -0,0 +1,23 @@
+import java.io.PrintStream;
+
+public class HelloWorld3 {
+    public static void main(String[] args) {
+        System.out.printf("Freitag, %d.%05d.2026\n", 15, 19);
+        System.out.printf("Freitag, %d.0%-2d.%d\n", 15, 8, 2026);
+        System.out.printf("Freitag, %d.%+-8d.%d\n", 15, 9, 2026);
+
+
+        /*System.out.printf("Freitag, %d.09.2017\n", 15);
+        System.out.printf("Freitag, %d.%d.%d\n", 15, 9, 2017);
+        System.out.printf("Freitag, %d.%2d.%d\n", 15, 9, 2017);
+        System.out.printf("Freitag, %d.%02d.%d\n", 15, 9, 2017);
+        System.out.printf("Freitag, %d.%-2d.%d\n", 15, 9, 2017);
+        System.out.printf("Freitag, %d.%+2d.%d\n", 15, 9, 2017);
+        System.out.printf("Freitag, %d.%+-8d.%d\n", 15, -9, 2017);
+        System.out.printf("Freitag, %02d.%02d.%4d\n\n", 15, 9, 2017);*/
+
+                }
+
+
+
+}
